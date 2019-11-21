d67f8f675066a84467e2dad380737e7c146453e7
Emmanuel De Saint Steban <emmanueldesaintsteban@gmail.com>
2019-11-21 21:22:43 +0000

    feat(autospy) can define custom spy
    changes autospy functions to have a switch where it could be possible to define specific project spy.

--- auto-spy.ts
+++ auto-spy.ts
@@ -19,9 +19,7 @@ export function autoSpy<T>(obj: any, type: string = ''): SpyOf<T> {
 function defaultSpy<t>(obj: new (...args: any[]) => T) {
     const res: SpyOf<T> = {} as any;
 
-    // turns out that in target:es2015 the methods attached to the prototype are not enumerable so Object.keys returns []. So to workaround that and keep some backwards compatibility - merge with ownPropertyNames - that disregards the enumerable property.
-    const keys = [...Object.keys(obj.prototype), ...Object.getOwnPropertyNames(obj.prototype)];
-    keys.forEach(key => {
+    Object.keys(obj.prototype).forEach(key => {
         res[key] = jasmine.createSpy(key);
     });
 
